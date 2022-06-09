6653054698d2e564392f9487f95e34b1d97bb4fd
frostzzone <65735427+frostzzone@users.noreply.github.com>
2022-06-09 00:17:54 +0000

    credit where due

--- src/components/NavigationBar/Credit.vue
+++ src/components/NavigationBar/Credit.vue
@@ -13,7 +13,7 @@ export default {
                 position: 'center',
                 icon: 'info',
                 //'Androz, snowflake107 - original s4d\n7coil - Discord blocks',
-                html: `<h3 style="background: -webkit-linear-gradient(60deg, #eee, #333);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">Original s4d by Androz, snowflake107<br>Discord Blocks by 7coil </h3>Scratch for discord PY is maintained by:
+                html: `<h3 style="background: -webkit-linear-gradient(60deg, #eee, #333);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">Original s4d by Androz, snowflake107<br>Discord Blocks by 7coil<br>JermeyGamer13 for search category smarts </h3>Scratch for discord PY is maintained by:
                   <span style="color:#2aa9bd">frostzzone</span>,
                   <span style="color:#ff7b00">Parham</span>`,
                 showConfirmButton: true
